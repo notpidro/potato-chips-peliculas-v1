da00b683a66fb53497e9e322a3ae77a29a71ec59
Walter F. Campillay P <walterfabian.cp@hotmail.com>
2023-06-30 19:36:39 -0300

    Agrego manejo de Reviews

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
 import Trailer from "./components/trailer/Trailer";
+import Reviews from "./components/reviews/Reviews";
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
 
   const getMovies = async () =>{
 
@@ -22,7 +25,23 @@ function App() {
     } catch(err){
       console.log(err);
     }
-    
+
+  }
+
+  const getMovieData = async (movieId) => {
+
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviews);
+
+    } catch (error) {
+      console.error(err)
+    }
   }
 
   useEffect(() => {
@@ -36,6 +55,7 @@ function App() {
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} reviews={reviews} setReviews={setReviews}/>}></Route>
         </Route>
       </Routes>
 

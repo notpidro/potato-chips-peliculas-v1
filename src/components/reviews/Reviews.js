2889fea5d5425c1de909eb656056d082f843b17f
Walter F. Campillay P <walterfabian.cp@hotmail.com>
2023-06-30 19:37:13 -0300

    Manejo y estilado de las reviews

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -6,12 +6,86 @@ import ReviewForm from "../reviewForm/ReviewForm";
 
 import React from "react";
 
-const Reviews = () => {
-    return (
-        <div>
+const Reviews = ({getMovieData, movie, reviews, setReviews}) => {
 
-        </div>
-    )
+    const revText = useRef();
+    let params = useParams();
+    const movieId = params.movieId;
+
+    useEffect(() => {
+        getMovieData(movieId);
+    },[])
+
+    const addReview = async (e) => {
+        e.preventDefault();
+
+        const rev = revText.current;
+
+        try {
+
+        } catch (err) {
+            console.error(err)
+        }
+
+        const response = await api.post("/api/v1/reviews", {reviewBody:rev.value, imdbId:movieId});
+
+        const updateReviews = [...reviews,{body:rev.value}];
+
+        rev.value = "";
+
+        setReviews(updateReviews);
+
+    }
+
+    return (
+		<Container>
+			<Row>
+				<Col>
+					<h3>Reviews</h3>
+				</Col>
+			</Row>
+			<Row className="mt-2">
+				<Col>
+					<img src={movie?.poster} alt="" />
+				</Col>
+				<Col>
+					{
+						<>
+							<Row>
+								<Col>
+									<ReviewForm handleSubmit={addReview} revText={revText} labelText="Escribe tu review" />
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+									<hr />
+								</Col>
+							</Row>
+						</>
+					}
+					{reviews?.map((r) => {
+						return (
+							<>
+								<Row>
+									<Col>{r.body}</Col>
+								</Row>
+								<Row>
+									<Col>
+										<hr />
+									</Col>
+								</Row>
+							</>
+						);
+					})}
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<hr />
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default Reviews

abbf70f29550ac47f8774feda0195ca2d9d2af00
Walter F. Campillay P <walterfabian.cp@hotmail.com>
2023-06-30 21:48:25 -0300

    Agrego boton para reviews

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,9 +3,17 @@ import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const Hero = ({movies}) => {
+
+    const navigate = useNavigate();
+
+    function reviews(movieId) {
+        navigate(`/Reviews/${movieId}`);
+    }
+
     return (
         <div className="contenedor-carrusel-peliculas">
             <Carousel>
@@ -30,6 +38,9 @@ const Hero = ({movies}) => {
                                                         />
                                                     </div>
                                                 </Link>
+                                                <div className="contenedor-boton-review">
+                                                    <Button variant="info" onClick={() => reviews(movie.imdbId)}>Reviews</Button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
